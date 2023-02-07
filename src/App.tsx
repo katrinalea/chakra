5491de5f7bad69457d88ba92d77f54f06146655e
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-02-07 13:53:02 +0000

    chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { greet } from "./utils/greet";
+import { Button, ChakraProvider, Heading } from "@chakra-ui/react";
+
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+  <ChakraProvider>
+    <Heading colorScheme='facebook'> Heading</Heading>
+  <Button colorScheme='telegram'>Hello</Button>
+  </ChakraProvider>
+  )
 }
 
 export default App;
